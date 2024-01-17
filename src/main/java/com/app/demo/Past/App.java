570ddc7aef3e82c246acd153b2ad2666f476d9a2
divyamull <divyasaimullapudi@gmail.com>
2024-01-17 22:39:13 +0530

    mvjvpu

---
+++ src/main/java/com/app/demo/Past/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Past;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
